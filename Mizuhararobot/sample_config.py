37c4e37be18d10b350f32a15d8a394496dc918c0
StreamFlix <90449809+StreamFlixGit@users.noreply.github.com>
2021-12-10 18:37:33 +0530

    Update sample_config.py

--- Mizuhararobot/sample_config.py
+++ Mizuhararobot/sample_config.py
@@ -14,12 +14,12 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    API_ID = 7832858  # integer value, dont use ""
+    API_HASH = "9707c9f17b2b88a1a0ac2bab55d2749b"
+    TOKEN = "5099985849:AAGFv-kMTBNC0vZv0t43ltSh4_eu6s1SfgU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 2132328048  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "MikuNakanoOfficial"
+    SUPPORT_CHAT = "RimuruTempestGroup"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.

f59180edf6bc9c741703da1cc86a1fe14c3276f5
StreamFlix <90449809+StreamFlixGit@users.noreply.github.com>
2021-12-10 19:38:40 +0530

    Update sample_config.py

--- Mizuhararobot/sample_config.py
+++ Mizuhararobot/sample_config.py
@@ -21,10 +21,10 @@ class Config(object):
     OWNER_USERNAME = "MikuNakanoOfficial"
     SUPPORT_CHAT = "RimuruTempestGroup"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001721767919
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001721767919
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
